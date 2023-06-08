536dd450e217980d27fc624ed5d725ec6c798f75
ice <ice@xicex.fr>
2023-06-08 14:34:03 +0200

    Fix: Waze

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -7,10 +7,10 @@ const Switch = (): JSX.Element => {
     const themeContext = useContext(ThemeContext);
 
     return (
-        <p className={styles.switch} >
-            <input type="checkbox" checked={!themeContext?.isDarkMode} onChange={themeContext?.switchTheme} name='switch' aria-label='switch theme' />
+        <label className={styles.switch} htmlFor="switch">
+            <input type="checkbox" checked={!themeContext?.isDarkMode} onChange={themeContext?.switchTheme} id="switch" name='switch' aria-label='switch theme' />
             <span className={[styles.slider, themeContext?.isDarkMode ? styles['slider--dark'] : styles['slider--light']].join(' ')} ><i className="fa-solid fa-moon"></i><i className="fa-solid fa-sun"></i></span>
-        </p>
+        </label>
     );
 }
 
