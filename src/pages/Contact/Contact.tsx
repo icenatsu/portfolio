98a052fb7b834031ed49e6705e076296f0dd3268
ice <ice@xicex.fr>
2023-06-09 17:52:56 +0200

    Maj: pour leorio (pb input)

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -5,6 +5,12 @@ const Contact = (): JSX.Element => {
     return (
         <div className={styles.container}>
             <Banner></Banner>
+            <div className={styles.form__contact}>
+                <label className={styles.lastname} htmlFor="lastName">Nom :</label>
+
+                <input className={styles.test} type="text" id="lastName" placeholder="tape ton nom" />
+
+            </div>
         </div>
     );
 };
