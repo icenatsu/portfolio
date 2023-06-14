6116645c8c4770a93c3375e940e1fa52ba027fac
ice <ice@xicex.fr>
2023-06-14 23:46:54 +0200

    remise a niveau

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -12,14 +12,31 @@ const NavBar = ({ inHandleClick }: NavBarProps): JSX.Element => {
     const themeContext = useContext(ThemeContext);
 
     const list = useRef<HTMLUListElement>(null)
+    const testsup = useRef<HTMLLIElement>(null)
 
     useEffect(() => {
-        themeContext?.darkLightMode(list.current, 'list', styles)
+
+        if (list.current !== null && testsup !== null) {
+            const classeDarkLightMode = [
+                {
+                    current: list.current,
+                    element: 'list',
+                    scss: styles
+                },
+                {
+                    current: testsup.current,
+                    element: 'testsup',
+                    scss: styles
+                }
+            ]
+
+            themeContext?.darkLightMode(classeDarkLightMode)
+        }
     }, [themeContext?.isDarkMode])
 
     return (
         <ul ref={list} id={styles.list} className={styles.list}>
-            <li className={styles.list__item} onClick={inHandleClick}><NavLink to="/">Accueil </NavLink></li>
+            <li ref={testsup} className={styles.list__item} onClick={inHandleClick}><NavLink to="/">Accueil </NavLink></li>
             <li className={styles.list__item} onClick={inHandleClick}><NavLink to="/projets">Projets</NavLink></li>
             <li className={styles.list__item} onClick={inHandleClick}><NavLink to="/experience">Expérience</NavLink></li>
             <li className={styles.list__item} onClick={inHandleClick}><NavLink to="/contact">Contact</NavLink></li>
